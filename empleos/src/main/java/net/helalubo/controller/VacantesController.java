cd50197f13ad17d2c22ff3d138b769fddcad43fb
Helalubo <alejandrodemoraiz@gmail.com>
2021-01-13 23:51:21 -0300

    agrego botton de borrar con request param y lo muestro en la vista mensaje. este evento sera lanzado cuando haga click en el boton en el listado de vacantes

--- empleos/src/main/java/net/helalubo/controller/VacantesController.java
+++ empleos/src/main/java/net/helalubo/controller/VacantesController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import net.helalubo.model.Vacante;
 
@@ -14,6 +15,21 @@ import net.helalubo.model.Vacante;
 @RequestMapping("/vacantes")
 public class VacantesController {
 
+	@GetMapping("/delete")
+	public String eliminar(@RequestParam("id") int idVancante, Model model) {
+		
+		
+		model.addAttribute("id",idVancante);
+		System.out.print("Borrando id " + idVancante);
+		
+		return "mensaje";
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	/*
