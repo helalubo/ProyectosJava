ca11ac631952a2e0320ccf5f55aba0fcd3b2f11e
Helalubo <alejandrodemoraiz@gmail.com>
2021-01-31 04:05:04 -0300

    Agrego clase en package config para poder configurar la ruta de recursos estaticos que esta ligada a la carpeta que utilizare como servidor, agrego consumo con el alias /logos/ a la vista para poder acceder a las imagenes desde dicha carpeta, tambien agrego logo de pagina en el frangment menu

---
+++ empleos/src/main/java/net/helalubo/config/WebConfig.java
@@ -0,0 +1,28 @@
+package net.helalubo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+ * 
+ * Creo clase de configuracion para poder cambiar la ruta de recursos estaticos,
+ * es este caso la ruta es una creada en d/empleos
+ * la cual esta ligada a la subida de imagenes
+ * 
+ * luego lo unico que se debe haces es usar el alias (en este  caso /logos/) 
+ * para poder acceder a las imagenes agregandole el nombre,
+ * 
+ * 
+ * */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// registry.addResourceHandler("/logos/**").addResourceLocations("file:
+		// /empleos/img-vacantes/"); // Linux
+		registry.addResourceHandler("/logos/**").addResourceLocations("file:d:/empleos/img-vacantes/"); // Windows
+	}
+
+}
